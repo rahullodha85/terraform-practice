815c7611ca6cdc384a2a764661fdf69b87f9e1aa
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2018-12-16 13:23:58 -0500

    file upload with provision, remote exec and connection

--- ec2-standalone/main.tf
+++ ec2-standalone/main.tf
@@ -1,4 +1,27 @@
 resource "aws_instance" "stand-alone" {
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "${var.INSTANCE_SIZE}"
+  key_name = "${aws_key_pair.my_aws_key.key_name}"
+
+  provisioner "file" {
+    source = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo /tmp/script.sh"
+    ]
+  }
+
+  connection {
+    user = "${var.INSTANCE_USER}"
+    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+  }
+}
+
+resource "aws_key_pair" "my_aws_key" {
+  key_name = "my_aws_key"
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
--- ec2-standalone/vars.tf
+++ ec2-standalone/vars.tf
@@ -6,11 +6,24 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-009d6802948d06e52"
+    us-east-1 = "ami-0ac019f4fcb7cb7e6"
+    us-east-2 = "ami-02e680c4540db351e"
   }
 }
 
 variable "INSTANCE_SIZE" {
   description = "aws instance size"
   default = "t2.micro"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "my_aws_key.pub"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "my_aws_key"
+}
+
+variable "INSTANCE_USER" {
+  default = "ubuntu"
 }
