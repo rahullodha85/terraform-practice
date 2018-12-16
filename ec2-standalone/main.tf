f81003c0c6872bdfdebdbda0d7d5e25bbbf528bd
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2018-12-16 13:29:39 -0500

    outputting instance public_ip

--- ec2-standalone/main.tf
+++ ec2-standalone/main.tf
@@ -25,3 +25,7 @@ resource "aws_key_pair" "my_aws_key" {
   key_name = "my_aws_key"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
+
+output "public_ip" {
+  value = "${aws_instance.stand-alone.public_ip}"
+}
