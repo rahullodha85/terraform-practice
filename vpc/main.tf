d97daecd66f5655e0e16c0dc6bc9364322aeeade
Rahul Lodha <rahul@152.sub-97-40-145.myvzw.com>
2019-01-02 10:30:36 -0500

    removed nat gateway

--- vpc/main.tf
+++ vpc/main.tf
@@ -109,40 +109,40 @@ resource "aws_route_table_association" "tf-public-1-c" {
 }
 
 # nat gw
-resource "aws_eip" "tf-nat" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "tf-nat-gw" {
-  allocation_id = "${aws_eip.tf-nat.id}"
-  subnet_id = "${aws_subnet.tf-main-public-1.id}"
-  depends_on = ["aws_internet_gateway.tf-main-gw"]
-}
-
-# VPC setup on nat
-resource "aws_route_table" "tf-main-private" {
-  vpc_id = "${aws_vpc.tf-main.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = "${aws_nat_gateway.tf-nat-gw.id}"
-  }
-  tags {
-    Name = "main-private-1"
-  }
-}
-
-# route associations private
-resource "aws_route_table_association" "tf-main-private-1-a" {
-  route_table_id = "${aws_route_table.tf-main-private.id}"
-  subnet_id = "${aws_subnet.tf-main-private-1.id}"
-}
-
-resource "aws_route_table_association" "tf-main-private-1-b" {
-  route_table_id = "${aws_route_table.tf-main-private.id}"
-  subnet_id = "${aws_subnet.tf-main-private-2.id}"
-}
-
-resource "aws_route_table_association" "tf-main-private-1-c" {
-  route_table_id = "${aws_route_table.tf-main-private.id}"
-  subnet_id = "${aws_subnet.tf-main-private-3.id}"
-}
+//resource "aws_eip" "tf-nat" {
+//  vpc = true
+//}
+//
+//resource "aws_nat_gateway" "tf-nat-gw" {
+//  allocation_id = "${aws_eip.tf-nat.id}"
+//  subnet_id = "${aws_subnet.tf-main-public-1.id}"
+//  depends_on = ["aws_internet_gateway.tf-main-gw"]
+//}
+//
+//# VPC setup on nat
+//resource "aws_route_table" "tf-main-private" {
+//  vpc_id = "${aws_vpc.tf-main.id}"
+//  route {
+//    cidr_block = "0.0.0.0/0"
+//    nat_gateway_id = "${aws_nat_gateway.tf-nat-gw.id}"
+//  }
+//  tags {
+//    Name = "main-private-1"
+//  }
+//}
+//
+//# route associations private
+//resource "aws_route_table_association" "tf-main-private-1-a" {
+//  route_table_id = "${aws_route_table.tf-main-private.id}"
+//  subnet_id = "${aws_subnet.tf-main-private-1.id}"
+//}
+//
+//resource "aws_route_table_association" "tf-main-private-1-b" {
+//  route_table_id = "${aws_route_table.tf-main-private.id}"
+//  subnet_id = "${aws_subnet.tf-main-private-2.id}"
+//}
+//
+//resource "aws_route_table_association" "tf-main-private-1-c" {
+//  route_table_id = "${aws_route_table.tf-main-private.id}"
+//  subnet_id = "${aws_subnet.tf-main-private-3.id}"
+//}
