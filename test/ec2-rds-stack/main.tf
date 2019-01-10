3b81624ac76f68a6429bfafa9ef951293c05826b
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-01-09 21:47:49 -0500

    removed tfvars file and moved ec2-rds-stack under test dir

--- test/ec2-rds-stack/main.tf
+++ test/ec2-rds-stack/main.tf
@@ -1,5 +1,5 @@
 module "ec2-instance" {
-  source = "./../ec2-standalone"
+  source = "./../../ec2-standalone"
   SUBNET_ID = "${module.vpc.subnet-public-1a-id}"
   VPC_SECURITY_GRPS = ["${module.vpc.ec2-instance-security-grp}"]
   CLOUD_INIT_TEMPLATE = "${module.storage.cloudinit-storage-template}"
@@ -10,21 +10,21 @@ module "ec2-instance" {
 }
 
 module "vpc" {
-  source = "./../vpc"
+  source = "./../../vpc"
 }
 
 module "storage" {
-  source = "./../storage"
+  source = "./../../storage"
 }
 
 module "storage-attachment" {
-  source = "./../storage-attachment"
+  source = "./../../storage-attachment"
   EBS_VOLUME_ID = "${module.storage.ebs-volume-id}"
   EC2_INSTANCE_ID = "${module.ec2-instance.id}"
 }
 
 module "rds" {
-  source = "./../rds"
+  source = "./../../rds"
   MARIADB_PASSWORD = "${var.MARIADB_PASSWORD}"
   MARIADB_SECURITY_GRP_ID = ["${module.vpc.mariadb-security-grp-id}"]
   PREFERRED_MARIADB_AVAILABILITY_ZONE = "${module.vpc.subnet-private-1a-availability-zone}"
@@ -32,6 +32,6 @@ module "rds" {
 }
 
 module "key" {
-  source = "./../key"
+  source = "./../../key"
   PATH_TO_PUBLIC_KEY = "${var.PATH_TO_PUBLIC_KEY}"
 }
