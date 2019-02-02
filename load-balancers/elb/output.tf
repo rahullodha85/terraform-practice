10d72566b0228ee50fcf4e4f35ed16197fab0b69
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-02-02 13:24:58 -0500

    output values for load balancer dns-name

--- load-balancers/elb/output.tf
+++ load-balancers/elb/output.tf
@@ -1,3 +1,7 @@
 output "load_balancer_name" {
   value = "${aws_elb.my-elb.name}"
 }
+
+output "load_balancer_dns_name" {
+  value = "${aws_elb.my-elb.dns_name}"
+}
---
+++ test/load-balancers/elb-test/output.tf
@@ -0,0 +1,7 @@
+output "load_balancer_url" {
+  value = "${module.elb.load_balancer_name}"
+}
+
+output "load_balancer_dns_name" {
+  value = "${module.elb.load_balancer_dns_name}"
+}
