f0b9254c0334a981f3e0d0db2be84489357be788
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-02-02 20:11:11 -0500

    changing security grp

--- test/ec2-rds-stack/main.tf
+++ test/ec2-rds-stack/main.tf
@@ -1,9 +1,9 @@
 module "ec2-instance" {
   source = "./../../ec2-standalone"
   SUBNET_ID = "${module.vpc.subnet-public-1a-id}"
-  VPC_SECURITY_GRPS = ["${module.vpc.ec2-instance-security-grp}"]
+  VPC_SECURITY_GRPS = ["${module.ec2-security-grp.id}"]
   CLOUD_INIT_TEMPLATE = "${module.storage.cloudinit-storage-template}"
-  SECURITY_GRPS = ["${module.vpc.ec2-instance-security-grp}"]
+  SECURITY_GRPS = ["${module.ec2-security-grp.id}"]
   EBS_VOLUME_ID = "${module.storage.ebs-volume-id}"
   AWS_REGION = "${var.AWS_REGION}"
   KEY_NAME = "${module.key.key_name}"
@@ -26,7 +26,7 @@ module "storage-attachment" {
 module "rds" {
   source = "./../../rds"
   MARIADB_PASSWORD = "${var.MARIADB_PASSWORD}"
-  MARIADB_SECURITY_GRP_ID = ["${module.vpc.mariadb-security-grp-id}"]
+  MARIADB_SECURITY_GRP_ID = ["${module.rds-security-grp.id}"]
   PREFERRED_MARIADB_AVAILABILITY_ZONE = "${module.vpc.subnet-private-1a-availability-zone}"
   MARIADB_SECURITY_GRP_ID_LIST = ["${module.vpc.subnet-private-1a-id}", "${module.vpc.subnet-private-1b-id}"]
 }
