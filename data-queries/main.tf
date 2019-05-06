f81a4aa7db405339ab408d004509eb83b71b522a
Rahul Lodha <rahul@152.sub-97-40-145.myvzw.com>
2019-05-06 18:20:44 -0400

    ec2 scaling with count code example

---
+++ data-queries/main.tf
@@ -0,0 +1,18 @@
+data "aws_vpc" "default" {
+  filter {
+    name = "tag:Name"
+    values = ["tf-main"]
+  }
+}
+
+data "aws_subnet" "subnet" {
+  count = "${var.COUNT}"
+  availability_zone = "${element(data.aws_availability_zones.availability-zones.names, count.index)}"
+  tags {
+    "Mode" = "public"
+  }
+}
+
+data "aws_availability_zones" "availability-zones" {
+  state = "available"
+}
---
+++ data-queries/output.tf
@@ -0,0 +1,11 @@
+output "vpc-main" {
+  value = "${data.aws_vpc.default.id}"
+}
+
+output "tf-vpc-subnet-public" {
+  value = "${data.aws_subnet.subnet.*.id}"
+}
+
+output "availability-zones" {
+  value = "${data.aws_availability_zones.availability-zones.names}"
+}
