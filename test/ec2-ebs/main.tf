d485ac7e46a5df19024520391376a0df5dd5cc6f
Rahul Lodha <rahul@146.sub-97-32-143.myvzw.com>
2019-05-07 11:08:33 -0400

    added scalability with instance_count

--- test/ec2-ebs/main.tf
+++ test/ec2-ebs/main.tf
@@ -1,5 +1,10 @@
-module "vpc" {
-  source = "./../../vpc"
+//module "vpc" {
+//  source = "./../../vpc"
+//}
+
+module "data-queries" {
+  source = "../../data-queries"
+  COUNT = "${var.INSTANCE_COUNT}"
 }
 
 module "ec2-instance" {
@@ -7,20 +12,23 @@ module "ec2-instance" {
   VPC_SECURITY_GRPS = ["${module.security-grp.id}"]
   AWS_REGION = "us-east-1"
   USER_DATA = "${module.ebs.cloudinit-storage-template}"
-  EBS_VOLUME_ID = "${module.ebs.ebs-volume-id}"
-  SECURITY_GRPS = ["${module.security-grp.id}"]
   KEY_NAME = "${module.key.key_name}"
-  SUBNET_ID = "${module.vpc.subnet-public-1a-id}"
+  SUBNET_ID = "${module.data-queries.tf-vpc-subnet-public}"
+  INSTANCE_COUNT = "${var.INSTANCE_COUNT}"
+  AVAILABILITY_ZONE = "${module.data-queries.availability-zones}"
 }
 
 module "ebs" {
   source = "./../../storage"
+  AVAILABILITY_ZONE = "${module.data-queries.availability-zones}"
+  INSTANCE_COUNT = "${var.INSTANCE_COUNT}"
 }
 
 module "storage-attachment" {
   source = "./../../storage-attachment"
   EBS_VOLUME_ID = "${module.ebs.ebs-volume-id}"
   EC2_INSTANCE_ID = "${module.ec2-instance.id}"
+  INSTANCE_COUNT = "${var.INSTANCE_COUNT}"
 }
 
 module "key" {
@@ -29,7 +37,7 @@ module "key" {
 
 module "security-grp" {
   source = "./../../security-group"
-  VPC_ID = "${module.vpc.vpc_id}"
+  VPC_ID = "${module.data-queries.vpc-main}"
   SECURITY_GRP_DESCRIPTION = "ec2-instance security group"
   SECURITY_GRP_NAME = "ec2-security-grp"
 }
@@ -60,4 +68,8 @@ terraform {
     key = "terraform/ec2-ebs"
     region = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = "${var.AWS_REGION}"
 }
---
+++ test/ec2-ebs/vars.tf
@@ -0,0 +1,7 @@
+variable "INSTANCE_COUNT" {
+  default = 1
+}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
