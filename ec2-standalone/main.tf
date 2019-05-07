548985901ce90b2ddb3c62a7986854d4a97b2f7a
Rahul Lodha <rahul@146.sub-97-32-143.myvzw.com>
2019-05-07 11:13:16 -0400

    ec2 instance scalability added with instance_count

--- ec2-standalone/main.tf
+++ ec2-standalone/main.tf
@@ -1,27 +1,27 @@
 resource "aws_instance" "stand-alone" {
-  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type = "${var.INSTANCE_SIZE}"
-  key_name      = "${var.KEY_NAME}"
-  subnet_id     = "${var.SUBNET_ID[count.index]}"
+  ami                    = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type          = "${var.INSTANCE_SIZE}"
+  key_name               = "${var.KEY_NAME}"
+  subnet_id              = "${var.SUBNET_ID[count.index]}"
   vpc_security_group_ids = ["${var.VPC_SECURITY_GRPS}"]
-  user_data = "${var.USER_DATA}"
-  availability_zone = "${element(var.AVAILABILITY_ZONE, count.index)}"
-  count = "${var.COUNT}"
+  user_data              = "${var.USER_DATA}"
+  availability_zone      = "${element(var.AVAILABILITY_ZONE, count.index)}"
+  count                  = "${var.INSTANCE_COUNT}"
 
-//  provisioner "file" {
-//    source      = "${path.module}/script.sh"
-//    destination = "/tmp/script.sh"
-//  }
-//
-//  provisioner "remote-exec" {
-//    inline = [
-//      "chmod +x /tmp/script.sh",
-//      "sudo /tmp/script.sh",
-//    ]
-//  }
-//
-//  connection {
-//    user        = "${var.INSTANCE_USER}"
-//    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
-//  }
+  //  provisioner "file" {
+  //    source      = "${path.module}/script.sh"
+  //    destination = "/tmp/script.sh"
+  //  }
+  //
+  //  provisioner "remote-exec" {
+  //    inline = [
+  //      "chmod +x /tmp/script.sh",
+  //      "sudo /tmp/script.sh",
+  //    ]
+  //  }
+  //
+  //  connection {
+  //    user        = "${var.INSTANCE_USER}"
+  //    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+  //  }
 }
--- ec2-standalone/vars.tf
+++ ec2-standalone/vars.tf
@@ -4,6 +4,7 @@ variable "AWS_REGION" {
 
 variable "AMIS" {
   type = "map"
+
   default = {
     us-east-1 = "ami-0ac019f4fcb7cb7e6"
     us-east-2 = "ami-02e680c4540db351e"
@@ -12,7 +13,7 @@ variable "AMIS" {
 
 variable "INSTANCE_SIZE" {
   description = "aws instance size"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
@@ -37,15 +38,9 @@ variable "VPC_SECURITY_GRPS" {
 
 variable "USER_DATA" {}
 
-//variable "SECURITY_GRPS" {
-//  type = "list"
-//}
-
-variable "EBS_VOLUME_ID" {}
-
 variable "KEY_NAME" {}
 
-variable "COUNT" {}
+variable "INSTANCE_COUNT" {}
 
 variable "AVAILABILITY_ZONE" {
   type = "list"
