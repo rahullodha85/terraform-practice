6f846e1a320d75c009fd3f403cd0e93a3a1cf796
Rahul Lodha <rahul@Rahuls-MBP.fios-router.home>
2019-07-30 23:40:49 -0400

    data queires + egress sec grp rule fixed

--- test/load-balancers/elb-test/main.tf
+++ test/load-balancers/elb-test/main.tf
@@ -1,11 +1,12 @@
 module "elb" {
   source = "./../../../load-balancers/elb"
   SECURITY_GRPS = ["${module.security_grp_elb.id}"]
-  SUBNETS = ["${module.vpc.subnet-public-1a-id}", "${module.vpc.subnet-public-1b-id}"]
+  SUBNETS = "${module.data-queries.tf-vpc-subnet-public}"//["${module.vpc.subnet-public-1a-id}", "${module.vpc.subnet-public-1b-id}"]
 }
 
-module "vpc" {
-  source = "./../../../vpc"
+module "data-queries" {
+  source = "../../../data-queries"
+  COUNT = "${var.INSTANCE_COUNT}"
 }
 
 module "key" {
@@ -16,17 +17,17 @@ module "security_grp_ec2" {
   source = "./../../../security-group"
   SECURITY_GRP_NAME = "ec2-instance-security-grp"
   SECURITY_GRP_DESCRIPTION = "ec2-instance secutiry group"
-  VPC_ID = "${module.vpc.vpc_id}"
+  VPC_ID = "${module.data-queries.vpc-main}"
 }
 
 module "security_grp_rule_ec2_egress" {
-  source = "./../../../security-group-rule/source_security_grp"
+  source = "./../../../security-group-rule/cidr"
   FROM_PORT = 0
   TO_PORT = 0
-  SOURCE_SECURITY_GRP_ID = "${module.security_grp_elb.id}"
   PROTOCOL = -1
   SECURITY_GRP_ID = "${module.security_grp_ec2.id}"
   TYPE = "egress"
+  CIDR_BLOCKS = ["0.0.0.0/0"]
 }
 
 module "security_grp_rule_ec2_ingress_ssh" {
@@ -53,7 +54,7 @@ module "security_grp_elb" {
   source = "./../../../security-group"
   SECURITY_GRP_NAME = "elb-security-grp"
   SECURITY_GRP_DESCRIPTION = "elb secutiry group"
-  VPC_ID = "${module.vpc.vpc_id}"
+  VPC_ID = "${module.data-queries.vpc-main}"
 }
 
 module "security_grp_rule_elb_egress" {
@@ -78,15 +79,15 @@ module "security_grp_rule_elb_ingress_web" {
 
 module "asg" {
   source = "./../../../asg"
-  VPC_ZONE_IDENTIFIER = ["${module.vpc.subnet-public-1b-id}", "${module.vpc.subnet-public-1a-id}"]
+  VPC_ZONE_IDENTIFIER = "${module.data-queries.tf-vpc-subnet-public}"
   SECURITY_GRPS = ["${module.security_grp_ec2.id}"]
   AWS_KEY = "${module.key.key_name}"
   LOAD_BALANCERS = ["${module.elb.load_balancer_name}"]
   AMI_ID = "${lookup(var.AMIS, var.AWS_REGION)}"
   FILE_NAME = "script.sh"
   HEALTHCHK_TYPE = "ELB"
-  MAX_SIZE = 2
-  MIN_SIZE = 2
+  MAX_SIZE = 3
+  MIN_SIZE = "${var.INSTANCE_COUNT}"
 }
 
 terraform {
@@ -95,4 +96,8 @@ terraform {
     key = "terraform/elb-asg"
     region = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = "${var.AWS_REGION}"
 }
