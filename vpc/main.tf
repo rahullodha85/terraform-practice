b24f9861dd9d438d436973f53172156fe6eaf92b
rlodha <rahul.lodha85@gmail.com>
2019-10-19 12:41:30 -0400

    vpc upgraded to terraform 12

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,144 +1,144 @@
 # Internet VPC
 resource "aws_vpc" "tf-main" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_support = "true"
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink = "false"
-  tags {
+  enable_classiclink   = "false"
+  tags = {
     Name = "tf-main"
   }
 }
 
 # Subnets
 resource "aws_subnet" "tf-main-public-1" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1a"
-  tags {
-    Name = "tf-main-public-1",
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "tf-main-public-1"
     Mode = "public"
   }
 }
 
 resource "aws_subnet" "tf-main-public-2" {
-  cidr_block = "10.0.2.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.2.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1b"
-  tags {
-    Name = "tf-main-public-2",
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "tf-main-public-2"
     Mode = "public"
   }
 }
 
 resource "aws_subnet" "tf-main-public-3" {
-  cidr_block = "10.0.3.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.3.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1c"
-  tags {
-    Name = "tf-main-public-3",
+  availability_zone       = "us-east-1c"
+  tags = {
+    Name = "tf-main-public-3"
     Mode = "public"
   }
 }
 
 resource "aws_subnet" "tf-main-public-4" {
-  cidr_block = "10.0.4.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.4.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1d"
-  tags {
-    Name = "tf-main-public-4",
+  availability_zone       = "us-east-1d"
+  tags = {
+    Name = "tf-main-public-4"
     Mode = "public"
   }
 }
 
 resource "aws_subnet" "tf-main-public-5" {
-  cidr_block = "10.0.5.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.5.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1e"
-  tags {
-    Name = "tf-main-public-5",
+  availability_zone       = "us-east-1e"
+  tags = {
+    Name = "tf-main-public-5"
     Mode = "public"
   }
 }
 
 resource "aws_subnet" "tf-main-public-6" {
-  cidr_block = "10.0.6.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.6.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1f"
-  tags {
-    Name = "tf-main-public-6",
+  availability_zone       = "us-east-1f"
+  tags = {
+    Name = "tf-main-public-6"
     Mode = "public"
   }
 }
 
 resource "aws_subnet" "tf-main-private-1" {
-  cidr_block = "10.0.7.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.7.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "false"
-  availability_zone = "us-east-1a"
-  tags {
-    Name = "tf-main-private-1",
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "tf-main-private-1"
     Mode = "private"
   }
 }
 
 resource "aws_subnet" "tf-main-private-2" {
-  cidr_block = "10.0.8.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.8.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "false"
-  availability_zone = "us-east-1b"
-  tags {
-    Name = "tf-main-public-2",
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "tf-main-public-2"
     Mode = "private"
   }
 }
 
 resource "aws_subnet" "tf-main-private-3" {
-  cidr_block = "10.0.9.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.9.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "false"
-  availability_zone = "us-east-1c"
-  tags {
-    Name = "tf-main-private-3",
+  availability_zone       = "us-east-1c"
+  tags = {
+    Name = "tf-main-private-3"
     Mode = "private"
   }
 }
 
 resource "aws_subnet" "tf-main-private-4" {
-  cidr_block = "10.0.10.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.10.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "false"
-  availability_zone = "us-east-1d"
-  tags {
-    Name = "tf-main-private-4",
+  availability_zone       = "us-east-1d"
+  tags = {
+    Name = "tf-main-private-4"
     Mode = "private"
   }
 }
 
 resource "aws_subnet" "tf-main-private-5" {
-  cidr_block = "10.0.11.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.11.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "false"
-  availability_zone = "us-east-1e"
-  tags {
-    Name = "tf-main-private-5",
+  availability_zone       = "us-east-1e"
+  tags = {
+    Name = "tf-main-private-5"
     Mode = "private"
   }
 }
 
 resource "aws_subnet" "tf-main-private-6" {
-  cidr_block = "10.0.12.0/24"
-  vpc_id = "${aws_vpc.tf-main.id}"
+  cidr_block              = "10.0.12.0/24"
+  vpc_id                  = aws_vpc.tf-main.id
   map_public_ip_on_launch = "false"
-  availability_zone = "us-east-1f"
-  tags {
-    Name = "tf-main-private-6",
+  availability_zone       = "us-east-1f"
+  tags = {
+    Name = "tf-main-private-6"
     Mode = "private"
   }
 }
@@ -146,19 +146,19 @@ resource "aws_subnet" "tf-main-private-6" {
 # Internet GW
 
 resource "aws_internet_gateway" "tf-main-gw" {
-  vpc_id = "${aws_vpc.tf-main.id}"
-  tags {
+  vpc_id = aws_vpc.tf-main.id
+  tags = {
     Name = "false"
   }
 }
 
 resource "aws_route_table" "tf-main-public" {
-  vpc_id = "${aws_vpc.tf-main.id}"
+  vpc_id = aws_vpc.tf-main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.tf-main-gw.id}"
+    gateway_id = aws_internet_gateway.tf-main-gw.id
   }
-  tags {
+  tags = {
     Name = "main-public-1"
   }
 }
@@ -166,33 +166,33 @@ resource "aws_route_table" "tf-main-public" {
 # route association public
 
 resource "aws_route_table_association" "tf-public-1-a" {
-  route_table_id = "${aws_route_table.tf-main-public.id}"
-  subnet_id = "${aws_subnet.tf-main-public-1.id}"
+  route_table_id = aws_route_table.tf-main-public.id
+  subnet_id      = aws_subnet.tf-main-public-1.id
 }
 
 resource "aws_route_table_association" "tf-public-1-b" {
-  route_table_id = "${aws_route_table.tf-main-public.id}"
-  subnet_id = "${aws_subnet.tf-main-public-2.id}"
+  route_table_id = aws_route_table.tf-main-public.id
+  subnet_id      = aws_subnet.tf-main-public-2.id
 }
 
 resource "aws_route_table_association" "tf-public-1-c" {
-  route_table_id = "${aws_route_table.tf-main-public.id}"
-  subnet_id = "${aws_subnet.tf-main-public-3.id}"
+  route_table_id = aws_route_table.tf-main-public.id
+  subnet_id      = aws_subnet.tf-main-public-3.id
 }
 
 resource "aws_route_table_association" "tf-public-1-d" {
-  route_table_id = "${aws_route_table.tf-main-public.id}"
-  subnet_id = "${aws_subnet.tf-main-public-4.id}"
+  route_table_id = aws_route_table.tf-main-public.id
+  subnet_id      = aws_subnet.tf-main-public-4.id
 }
 
 resource "aws_route_table_association" "tf-public-1-e" {
-  route_table_id = "${aws_route_table.tf-main-public.id}"
-  subnet_id = "${aws_subnet.tf-main-public-5.id}"
+  route_table_id = aws_route_table.tf-main-public.id
+  subnet_id      = aws_subnet.tf-main-public-5.id
 }
 
 resource "aws_route_table_association" "tf-public-1-f" {
-  route_table_id = "${aws_route_table.tf-main-public.id}"
-  subnet_id = "${aws_subnet.tf-main-public-6.id}"
+  route_table_id = aws_route_table.tf-main-public.id
+  subnet_id      = aws_subnet.tf-main-public-6.id
 }
 
 //# nat gw
--- vpc/output.tf
+++ vpc/output.tf
@@ -1,53 +1,53 @@
 output "vpc_id" {
-  value = "${aws_vpc.tf-main.id}"
+  value = aws_vpc.tf-main.id
 }
 
 output "subnet-public-1a-id" {
-  value = "${aws_subnet.tf-main-public-1.id}"
+  value = aws_subnet.tf-main-public-1.id
 }
 
 output "subnet-public-1b-id" {
-  value = "${aws_subnet.tf-main-public-2.id}"
+  value = aws_subnet.tf-main-public-2.id
 }
 
 output "subnet-public-1c-id" {
-  value = "${aws_subnet.tf-main-public-3.id}"
+  value = aws_subnet.tf-main-public-3.id
 }
 
 output "subnet-private-1a-id" {
-  value = "${aws_subnet.tf-main-private-1.id}"
+  value = aws_subnet.tf-main-private-1.id
 }
 
 output "subnet-private-1b-id" {
-  value = "${aws_subnet.tf-main-private-2.id}"
+  value = aws_subnet.tf-main-private-2.id
 }
 
 output "subnet-private-1c-id" {
-  value = "${aws_subnet.tf-main-private-3.id}"
+  value = aws_subnet.tf-main-private-3.id
 }
 
 output "subnet-public-1a-availability-zone" {
-  value = "${aws_subnet.tf-main-public-1.availability_zone}"
+  value = aws_subnet.tf-main-public-1.availability_zone
 }
 
 output "subnet-public-1b-availability-zone" {
-  value = "${aws_subnet.tf-main-public-2.availability_zone}"
+  value = aws_subnet.tf-main-public-2.availability_zone
 }
 
 output "subnet-public-1c-availability-zone" {
-  value = "${aws_subnet.tf-main-public-3.availability_zone}"
+  value = aws_subnet.tf-main-public-3.availability_zone
 }
 
 output "subnet-private-1a-availability-zone" {
-  value = "${aws_subnet.tf-main-private-1.availability_zone}"
+  value = aws_subnet.tf-main-private-1.availability_zone
 }
 
 output "subnet-private-1b-availability-zone" {
-  value = "${aws_subnet.tf-main-private-2.availability_zone}"
+  value = aws_subnet.tf-main-private-2.availability_zone
 }
 
 output "subnet-private-1c-availability-zone" {
-  value = "${aws_subnet.tf-main-private-3.availability_zone}"
+  value = aws_subnet.tf-main-private-3.availability_zone
 }
 
 //output "ec2-instance-security-grp" {
